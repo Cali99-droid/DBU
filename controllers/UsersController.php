68537540ab22c596c31d8dc4911e724194bd8f65
Papadopopo <83546594+Papadopopo@users.noreply.github.com>
2021-05-02 12:53:55 -0500

    Controlador de usuarios comentado

--- controllers/UsersController.php
+++ controllers/UsersController.php
@@ -1,19 +1,23 @@
 <?php 
 class UsersController {
 	private $model;
-
+	
+	//Función que permite instanciar un nuevo modelo de usuario
 	public function __construct() {
 		$this->model = new UsersModel();
 	}
-
+	//Función que permite ingresar datos de un usuario desde el modelo a la vista
+	//Parámetros: Conjunto de datos específicos de un usuario
 	public function set( $user_data = array() ) {
 		return $this->model->set($user_data);
 	}
-
+	//Función que permite obtener datos de un usuario desde la vista al modelo
+	//Parámetros: ID de usuario
 	public function get( $user_id = '' ) {
 		return $this->model->get($user_id);
 	}
-
+	//Función que permite eliminar datos de un usuario
+	//Parámetros: ID de usuario
 	public function del( $user_id = '' ) {
 		return $this->model->del($user_id);
 	}
