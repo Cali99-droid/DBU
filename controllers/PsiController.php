3e3143a89ee03346a359bd76a91ca4681d92d80c
Papadopopo <83546594+Papadopopo@users.noreply.github.com>
2021-05-02 12:26:07 -0500

    Controlador psicológico comentado

--- controllers/PsiController.php
+++ controllers/PsiController.php
@@ -2,23 +2,27 @@
 //require_once("C:/wamp64/www/Proyectos/DBU/models/HistorialPsicologico.php");
 class PsiController {
 	private $model;
-
+	//Función que instacia un nuevo historial psicológico
 	public function __construct() {
 		$this->model = new  HistorialPsicologico();
 	}
-
+	//Función que envía los datos del modelo a la vista de un historial psicológico
+	//Parámetros: Conjuntos de datos de un historial psicológico
 	public function set( $psicologia_data = array() ) {
 		return $this->model->set($psicologia_data);
 	}
-
+	//Función que obtiene los datos de un historial psicológico desde su vista hasta el modelo
+	//Parámetros: ID de historial psicológico
 	public function get( $psicologia_id = '' ) {
 		return $this->model->get($psicologia_id);
 	}
-
+	//Función que permite eliminar un historial psicológico 
+	//Parámetros: ID de historial psicológico
 	public function del( $psicologia_id = '' ) {
 		return $this->model->del($psicologia_id);
 	}
-
+	//Función que permite modificar datos de un historial psicológico 
+	//Parámetros: Conjuntos de datos de un historial psicológico
 	public function update( $psicologia_data = array() ) {
 		return $this->model->update($psicologia_data);
 	}
